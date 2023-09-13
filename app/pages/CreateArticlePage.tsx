9b493ec32eec19bf3a6de139de40cf4dc11d6b11
Artem <artemdefault6@gmail.com>
2023-09-13 20:22:38 +0200

    implemented CreateArticle

---
+++ app/pages/CreateArticlePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import CreateArticle from "~/components/BlogPage/CreateArticle";
+
+const CreateArticlePage = () => {
+  return (
+    <div>
+      <CreateArticle />
+    </div>
+  );
+};
+
+export default CreateArticlePage;
