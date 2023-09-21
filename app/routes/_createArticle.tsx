69c930114859b40c638078689a702527e5bd26a6
Artem <artemdefault6@gmail.com>
2023-09-21 17:53:49 +0200

    Revert "article creator"
    This reverts commit 9b1c4d9c46f1337d693bb7ba5e105164f183759b.

--- app/routes/_createArticle.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import CreateArticle from "~/components/BlogPage/CreateArticle";
-
-const CreateArticlePage = () => {
-  return <CreateArticle />;
-};
-
-export default CreateArticlePage;
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Layout } from "~/components/layout";
-import CreateArticle from "~/components/BlogPage/CreateArticle";
+import HomePage from "~/pages/HomePage";
 
 export default function Index() {
   return (
     <Layout>
-      <CreateArticle />
+      <HomePage />
     </Layout>
   );
 }
