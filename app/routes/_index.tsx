4d248aecab5bc1f036ef6be89f07946780f06be5
Artem <artemdefault6@gmail.com>
2023-09-21 16:49:08 +0200

    refactoring

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import { Layout } from "~/components/layout";
 import HomePage from "~/pages/HomePage";
-import React from "react";
+
 export default function Index() {
   return (
     <Layout>
