623cdf86589e23df31d4b5711e675767fdccd048
Artem <artemdefault6@gmail.com>
2023-10-03 14:15:13 +0200

    Revert "Topbar and footer"
    This reverts commit 40de069e69655d2ee824bedc075bae0acfeee0c0.

--- app/components/IconComponents/FacebookHeader.tsx
+++ app/components/IconComponents/FacebookHeader.tsx
@@ -10,8 +10,8 @@ const SvgFacebookHeader = (props: SVGProps<SVGSVGElement>) => (
     {...props}
   >
     <path
-      fill={props.fill}
-      fillOpacity={props.fillOpacity}
+      fill="#fff"
+      fillOpacity={0.7}
       d="M5.333 8H7l.667-2.667H5.333V4c0-.687 0-1.333 1.334-1.333h1V.427A18.778 18.778 0 0 0 5.762.333c-1.81 0-3.095 1.105-3.095 3.134v1.866h-2V8h2v5.667h2.666V8Z"
     />
   </svg>
